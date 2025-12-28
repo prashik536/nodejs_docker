ab01f1ca9f603490534a35fa1b029ae313ac5715
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2025-12-28 12:51:52 +0530

    Change greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App2!');
+  res.send('Hello from Dockerized Node.js App1!');
 });
 
 const PORT = process.env.PORT || 3000;
